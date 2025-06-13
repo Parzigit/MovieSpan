659c764f3cee6cb689043072275c14f8c6ac40b7
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-13 14:13:19 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
         <div className="header">
             <div><h1 style={{ fontSize: 25 }}>Movie<span>Span</span></h1></div>
             <div id="headernav">
-                <div><Link to="/index"><p id="headlink">Home</p></Link></div>
+                <div><Link to="/"><p id="headlink">Home</p></Link></div>
                 <div><Link to="/discover"><p id="headlink">Discover</p></Link></div>
             </div>
         </div>
