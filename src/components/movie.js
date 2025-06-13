3cd07a2d29d22779562ec6493520d6ffe51ab7cc
Parzigit <sumedhatreya007@gmail.com>
2025-06-13 14:02:29 +0530

    upd

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,6 +1,6 @@
 export const fetchData = async () => {
     try {
-        const API_KEY = process.env.REACT_APP_TMDB_KEY;
+        // const API_KEY = process.env.REACT_APP_TMDB_KEY;
         const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=f4384d2e71fe047b2aea1370308c02bb&language=en-US&page=1");
         const data = await response.json();
         console.log(data);
