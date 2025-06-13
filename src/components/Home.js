9ab2b94e3f768051d43949241549aced7af010c6
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-13 13:59:59 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
-import './styles/Home.css'
-import './styles/title.css'
+import './styles/Home.css';
+import './styles/title.css';
 
 export default function Home() {
     return (
-        <>
         <div className="homebg">
-            <h1 className="hometitle" align="center">This Is Cinema</h1>
-            <button className="button"><Link to="/discover" style={{textDecoration: 'none', color: 'green'}}>Explore Now</Link></button>
+            <h1 className="hometitle">This Is Cinema</h1>
+            <button className="button">
+                <Link to="/discover" className="explore-link">Explore Now</Link>
+            </button>
         </div>
-        </>
-    )
+    );
 }
